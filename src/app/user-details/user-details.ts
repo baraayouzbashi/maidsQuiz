ec380eb4b12c624847534bd57ea8a3456e131b59
baraa <baraayouzbashi@gmail.com>
2020-04-24 20:51:32 +0300

    for local testing only

--- src/app/user-details/user-details.ts
+++ src/app/user-details/user-details.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'user-details',
@@ -10,16 +11,25 @@ import {ActivatedRoute, ParamMap} from '@angular/router';
 export class UserDetailsComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private location: Location
   ) { }
 
   id;
+  avatar = 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg';
+  first_name = 'john';
+  last_name = 'smith';
+  email = 'some@one.com';
   ngOnInit() {
     this.route.paramMap.subscribe(
     (params: ParamMap) => {
        this.id = params.get('id');
     }
   );
+  }
+
+  goBack = () => {
+    this.location.back();
   }
 
 }
